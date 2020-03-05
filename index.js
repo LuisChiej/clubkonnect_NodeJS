e0c88fa82a658a3088b4e3f12e4c3b229af5d5de
LuisChiej <luis.chiej.lc@gmail.com>
2020-03-05 23:26:11 +0100

    New

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var konnect_electricity_1 = require("./lib/konnect.electricity");
 var konnect_wallet_1 = require("./lib/konnect.wallet");
 var Clubkonnect = /** @class */ (function () {
     function Clubkonnect(user_id, api_key) {
+        this.USER_ID = user_id;
+        this.API_KEY = api_key;
+        this.onInit();
         this.Airtime = new konnect_airtime_1.Airtime(this.club);
         this.Mobiledata = new konnect_mobiledata_1.Mobiledata(this.club);
         this.Cable = new konnect_cable_1.Cable(this.club);
         this.Electricity = new konnect_electricity_1.Electricity(this.club);
         this.Wallet = new konnect_wallet_1.Wallet(this.club);
-        this.USER_ID = user_id;
-        this.API_KEY = api_key;
-        this.onInit();
     }
     Clubkonnect.prototype.onInit = function () {
         this.club = new club_1.Club(this.USER_ID, this.API_KEY);
