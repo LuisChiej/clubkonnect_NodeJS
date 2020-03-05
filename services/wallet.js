f5a75cf765afd71fce542c25de2b4ca3ece6dc51
LuisChiej <luis.chiej.lc@gmail.com>
2020-03-05 23:10:10 +0100

    Updated

--- services/wallet.js
+++ services/wallet.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports.balance = function (_club) {
     return new Promise(function (resolve, reject) {
-        _club.request('APIWalletBalanceV1', null)
+        _club.balance_request('APIWalletBalanceV1')
             .then(function (res) {
             resolve(res);
         })["catch"](function (err) {
